c9b29707bcbf8614589173216d18e11bcfe19960
Kunj gupta <kunjgupta54@gmail.com>
2023-07-19 09:29:11 +0530

    partner

---
+++ lib/doctor_details.dart
@@ -0,0 +1,278 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:partnerr/details.dart';
+import 'package:partnerr/homepage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+String? validateEmail(String? value) {
+  const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+      r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+      r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+      r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+      r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+      r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+      r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+  final regex = RegExp(pattern);
+
+  return value!.isNotEmpty && !regex.hasMatch(value)
+      ? 'Enter a valid email address'
+      : null;
+}
+
+String? validatemobile(String value) {
+  String patttern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value.length == 0) {
+    return 'Please enter mobile number';
+  } else if (!regExp.hasMatch(value)) {
+    return 'Please enter valid mobile number';
+  }
+  return null;
+}
+
+class doctordetails extends StatelessWidget {
+  doctordetails({super.key});
+  final mobilecontroller = TextEditingController();
+  final emailcontroller = TextEditingController();
+  final namecontroller = TextEditingController();
+ final registraioncontroller =TextEditingController();
+ final pricecontroller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 400,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(.2))
+                    ]),
+                child: TextFormField(
+                  controller: namecontroller,
+                  decoration: InputDecoration(
+                      hintText: 'Full Name',
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(.2))
+                    ]),
+                child: TextFormField(
+                  controller: emailcontroller,
+                  autovalidateMode: AutovalidateMode.always,
+                  validator: validateEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      hintText: 'Email Address',
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(.2))
+                    ]),
+                child: TextFormField(
+                  controller: mobilecontroller,
+                  autovalidateMode: AutovalidateMode.always,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: 'Contact number',
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(.2))
+                    ]),
+                child: TextFormField(
+                  controller: registraioncontroller,
+
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: 'Registraion number',
+                      prefixIcon: Icon(
+                        Icons.numbers,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(.2))
+                    ]),
+                child: TextFormField(
+                  controller: pricecontroller,
+inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: 'price for 1 hour',
+                      prefixIcon: Icon(
+                        Icons.currency_rupee,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.white, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              ElevatedButton(
+
+                style: ButtonStyle(
+
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.red)
+                        )
+                    )
+                ),
+
+                onPressed: () {
+                  CollectionReference collref =
+                  FirebaseFirestore.instance.collection('doctor details');
+                  collref.add({
+                    'name': namecontroller.text,
+                    'email': emailcontroller.text,
+                    'mobile': mobilecontroller.text,
+                    'registraion number':registraioncontroller.text,
+                    'price':(pricecontroller).value,
+                  });
+                  if (namecontroller.text != "" &&
+                      emailcontroller.text != "" &&
+                      mobilecontroller.text != ""&&registraioncontroller.text!="") {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) =>
+homepage(),
+                      ),);
+                  }
+                },
+
+                child: Text('Submit',style: TextStyle(
+                  fontSize: 20,
+                ),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/partner_doctor.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:partnerr/user_details.dart';
+
+import 'doctor_details.dart';
+
+class partnerordoctor extends StatefulWidget {
+  const partnerordoctor({super.key});
+
+  @override
+  State<partnerordoctor> createState() => _partnerordoctorState();
+}
+
+class _partnerordoctorState extends State<partnerordoctor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+
+
+        child: Column(
+
+          children: [
+            SizedBox(height: 500,),
+            ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.red)))),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => doctordetails(),
+                  ),
+                );
+              },
+              child: Text(
+                'Doctor',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.red)))),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => userdetails(),
+                  ),
+                );
+              },
+              child: Text(
+                'Partner',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
